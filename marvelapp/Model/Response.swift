fdfc1f8b5d6e40b2f6cb0095c6a62bc422db8ce2
Guille <gbcriado@gmail.com>
2019-03-17 20:51:29 +0100

    Parsing response successfuly

--- marvelapp/Model/Response.swift
+++ marvelapp/Model/Response.swift
@@ -13,16 +13,34 @@ For support, please feel free to contact me at https://www.linkedin.com/in/syeda
 
 import Foundation
 struct Json4Swift_Base : Codable {
-	let results : [Results]?
+	let code : Int?
+	let status : String?
+	let copyright : String?
+	let attributionText : String?
+	let attributionHTML : String?
+	let etag : String?
+	let data : Data?
 
 	enum CodingKeys: String, CodingKey {
 
-		case results = "results"
+		case code = "code"
+		case status = "status"
+		case copyright = "copyright"
+		case attributionText = "attributionText"
+		case attributionHTML = "attributionHTML"
+		case etag = "etag"
+		case data = "data"
 	}
 
 	init(from decoder: Decoder) throws {
 		let values = try decoder.container(keyedBy: CodingKeys.self)
-		results = try values.decodeIfPresent([Results].self, forKey: .results)
+		code = try values.decodeIfPresent(Int.self, forKey: .code)
+		status = try values.decodeIfPresent(String.self, forKey: .status)
+		copyright = try values.decodeIfPresent(String.self, forKey: .copyright)
+		attributionText = try values.decodeIfPresent(String.self, forKey: .attributionText)
+		attributionHTML = try values.decodeIfPresent(String.self, forKey: .attributionHTML)
+		etag = try values.decodeIfPresent(String.self, forKey: .etag)
+		data = try values.decodeIfPresent(Data.self, forKey: .data)
 	}
 
 }
