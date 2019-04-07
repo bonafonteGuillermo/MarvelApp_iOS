45007e0007fbfbb01854c4b5ec917b7e458110a5
Guille <gbcriado@gmail.com>
2019-04-07 12:35:41 +0200

    Painting info, comics, series data correctly

--- marvelapp/detail/controller/DetailViewController.swift
+++ marvelapp/detail/controller/DetailViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+struct Constants {
+    static let SECTION_INFO = 0
+    static let SECTION_COMIC = 1
+    static let SECTION_SERIES = 2
+}
+
 class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     var character : Results?
@@ -17,29 +23,48 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         addCharacterToFavourite(characterId: (character?.id!)!)
     }*/
     
-    
     @IBOutlet weak var detailTableView: UITableView!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         detailTableView.delegate = self
         detailTableView.dataSource = self
-        //labelCharacterName.text = self.character?.name
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        switch section {
+        case Constants.SECTION_INFO:
+            return 1
+        case Constants.SECTION_COMIC:
+            return (character?.comics?.items?.count)!
+        case Constants.SECTION_SERIES:
+            return (character?.series?.items?.count)!
+       default:
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
-        
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
-        
-        return cell
+        switch indexPath.section {
+        case Constants.SECTION_INFO:
+            let sectionInfoCell = tableView.dequeueReusableCell(withIdentifier: "sectionInfoCell", for: indexPath)
+            sectionInfoCell.textLabel?.text = self.character?.description
+            return sectionInfoCell
+        case Constants.SECTION_COMIC:
+            //"Section \(indexPath.section)
+            let sectionComicCell = tableView.dequeueReusableCell(withIdentifier: "sectionInfoCell", for: indexPath)
+            sectionComicCell.textLabel?.text = self.character?.comics?.items?[indexPath.row].name
+            return sectionComicCell
+        case Constants.SECTION_SERIES:
+            let sectionSeriesCell = tableView.dequeueReusableCell(withIdentifier: "sectionInfoCell", for: indexPath)
+            sectionSeriesCell.textLabel?.text = self.character?.series?.items?[indexPath.row].name
+            return sectionSeriesCell
+        default:
+            let defaultCell = tableView.dequeueReusableCell(withIdentifier: "sectionInfoCell", for: indexPath)
+            defaultCell.textLabel?.text = "-"
+            return defaultCell
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -47,7 +72,16 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
+        switch section {
+        case Constants.SECTION_INFO:
+            return "Info"
+        case Constants.SECTION_COMIC:
+            return "Comics"
+        case Constants.SECTION_SERIES:
+            return "Series"
+        default:
+            return ""
+        }
     }
     
     func addCharacterToFavourite(characterId : Int){
